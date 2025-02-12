35098ea58d0f93b9fbb363396629f1425b799938
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2025-02-11 22:32:34 -0300

    tabela produtos

--- bdOEEManual.sql
+++ bdOEEManual.sql
@@ -1,3 +1,11 @@
 CREATE DATABASE IF NOT EXISTS oeemanual;
 USE oeemanual;
 
+CREATE TABLE produto(
+	id MEDIUMINT(10000000) AUTO_INCREMENT,
+    descricao VARCHAR(255),
+    codigo CHAR(7) UNIQUE,
+    velocidade DOUBLE(10,3),
+    batelada BOOL,
+    PRIMARY KEY(id)
+);
