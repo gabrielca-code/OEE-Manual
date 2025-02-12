32e9ac216cad92b04a14af62404f3e3f31208da3
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2025-02-11 23:06:54 -0300

    alterando tabela maquinas e criando tabela estrutura

--- bdOEEManual.sql
+++ bdOEEManual.sql
@@ -1,9 +1,11 @@
 CREATE DATABASE IF NOT EXISTS oeemanual;
 USE oeemanual;
 
+DROP TABLE IF EXISTS estrutura;
+
 DROP TABLE IF EXISTS produto;
 CREATE TABLE IF NOT EXISTS produto(
-	id INT(255) AUTO_INCREMENT,
+	id INT AUTO_INCREMENT,
     descricao VARCHAR(255) NOT NULL,
     codigo CHAR(7) UNIQUE NOT NULL,
     PRIMARY KEY(id)
@@ -16,19 +18,43 @@ INSERT INTO produto VALUES
 SELECT * 
 FROM produto;
 
-DROP TABLE IF EXISTS equipamento;
-CREATE TABLE IF NOT EXISTS equipamento(
-	id INT(255) AUTO_INCREMENT,
+DROP TABLE IF EXISTS maquina;
+CREATE TABLE IF NOT EXISTS maquina(
+	id INT AUTO_INCREMENT,
     descricao VARCHAR(255) NOT NULL,
     tag CHAR(11) NOT NULL,
     batelada BOOLEAN,
     PRIMARY KEY(id)
 );
 
-INSERT INTO equipamento VALUES
+INSERT INTO maquina VALUES
 (NULL, 'CAM 2', 'EBS-ECT-02', FALSE),
 (NULL, 'COP 10', 'MAS-COP-10', FALSE);
 
 SELECT * 
-FROM equipamento;
+FROM maquina;
 
+CREATE TABLE IF NOT EXISTS estrutura(
+	idProduto INT NOT NULL,
+    idMaquina INT NOT NULL,
+    velocidade DOUBLE(11,3) NOT NULL,
+    FOREIGN KEY (idProduto) REFERENCES produto (id),
+    FOREIGN KEY (idMaquina) REFERENCES maquina (id),
+    PRIMARY KEY(idProduto, idMaquina)
+);
+
+INSERT INTO estrutura VALUES
+(1, 1, 5000),
+(1, 2, 100000),
+(2, 2, 60000);
+
+SELECT 
+	p.descricao AS descricaoProduto, 
+    p.codigo AS codigoProduto, 
+    m.descricao AS descricaoMaquina, 
+    m.tag AS tagMaquina, 
+    m.batelada AS ehBatelada, 
+    e.velocidade AS velocidadeProdutoEquipamento
+FROM produto AS p
+INNER JOIN estrutura AS e ON p.id = e.idProduto
+INNER JOIN maquina AS m ON e.idMaquina = m.id;
