c8c1d7335b60933c38ea87d8d606ec5b13bd76e0
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2025-02-15 14:08:13 -0300

    Iniciando Api Rest

---
+++ src/main/java/com/gabriel/OEEManual/OeeManualApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.OEEManual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OeeManualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OeeManualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/OEEManual/OeeManualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.OEEManual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OeeManualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
